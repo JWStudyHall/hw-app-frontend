daab78603460d345c03c148df92d8528e220bc24
JEStudyhall <jaieelizabeth@gmail.com>
2026-03-05 12:54:53 -0500

    services error catch

--- src/services/exerciseService.js
+++ src/services/exerciseService.js
@@ -1,16 +1,28 @@
 import api from "./apiConfig.js";
 
 export const getMuscleGroups = async () => {
-  const resp = await api.get("/muscle-groups/");
-  return resp.data;
+  try {
+    const resp = await api.get("/muscle-groups/");
+    return resp.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getExercises = async () => {
-  const resp = await api.get("/exercises/");
-  return resp.data;
+  try {
+    const resp = await api.get("/exercises/");
+    return resp.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getExerciseById = async (exerciseId) => {
-  const resp = await api.get(`/exercises/${exerciseId}/`);
-  return resp.data;
+  try {
+    const resp = await api.get(`/exercises/${exerciseId}/`);
+    return resp.data;
+  } catch (error) {
+    throw error;
+  }
 };
