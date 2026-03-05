d5174a2c2cc420acd41811cbeb918b09dd3b21b9
JEStudyhall <jaieelizabeth@gmail.com>
2026-03-05 15:12:39 -0500

    plan-details, plan-list

--- src/components/Plans/PlansList.jsx
+++ src/components/Plans/PlansList.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import { getPlans } from '../../services/planService.js'
+
 
 const PlansList = () => {
   return (
